59682401fb40197c5a180bc2472abbc813904c26
pavansk00 <85490004+pavansk00@users.noreply.github.com>
2021-06-07 22:04:13 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,14 @@
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tmp=10;
+		int a=0;
+		for(int i=0;i<=tmp;i++)
+		{
+		 a=i+a;	
+		}
+	System.out.println("addition is:"+a);
+	}
+	
+}
